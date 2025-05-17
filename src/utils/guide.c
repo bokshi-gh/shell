3f4a49c2ce7613cfdb3aba720a055ab8f351a115
devRajeshThapa <devrajeshthapa@gmail.com>
2025-05-17 19:50:20 +0000

    Enhance

--- src/utils/guide.c
+++ src/utils/guide.c
@@ -14,17 +14,17 @@ void display_guide(){
 	printf("\n");
 
 	printf("Rumi Shell Commands:\n");
-	printf("  clean               - Clear the terminal screen\n");
-	printf("  esc                 - Exit the shell\n");
-	printf("  list                - List files and directories in the current directory\n");
-	printf("  newfile <filename>  - Create an empty file with the given name\n");
-	printf("  delfile <filename>  - Delete the specified file\n");
-	printf("  newdir  <dirname>   - Create a new directory with the given name\n");
-	printf("  deldir  <dirname>   - Delete an empty directory with the given name\n");
-	printf("  print \"<text>\"    - Print the specified text to the terminal\n");
-	printf("  read <filename>     - Display the contents of the specified file\n");
-	printf("  goto <pathname>     - Change the current directory to the specified path\n);
-	printf("  <non_rumi_command>  - Executed as a system command (e.g., ls, gcc, vim)\n");
+	printf("  clean              - Clear the terminal screen\n");
+	printf("  esc                - Exit the shell\n");
+	printf("  list               - List files and directories in the current directory\n");
+	printf("  newfile <filename> - Create an empty file with the given name\n");
+	printf("  delfile <filename> - Delete the specified file\n");
+	printf("  newdir  <dirname>  - Create a new directory with the given name\n");
+	printf("  deldir  <dirname>  - Delete an empty directory with the given name\n");
+	printf("  print \"<text>\"   - Print the specified text to the terminal\n");
+	printf("  read <filename>    - Display the contents of the specified file\n");
+	printf("  goto <pathname>    - Change the current directory to the specified path\n);
+	printf("  <non_rumi_command> - Executed as a system command (e.g., ls, gcc, vim)\n");
 
 	printf("\n");
 }
