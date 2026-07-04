6bd36751813cab28c8698aa5c1fdd466dd21d0d9
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 22:19:29 +0545

    Update goto.c

--- src/commands/goto.c
+++ src/commands/goto.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
+
 #include "goto.h"
 
-extern char *cwd;
-
-void change_directory(char *path){
-	if(chdir(path) == -1) perror("chdir");
-	if((getcwd(cwd, PATH_MAX)) == NULL){
-		perror("cwd");
-		exit(1);
-	}
-	return;
+void change_directory(char *path) {
+    if (chdir(path) == -1) {
+        perror("chdir");
+    }
 }
