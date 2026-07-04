295b5e840acc1a0d1f3df3bad42394961b06cc28
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 22:42:42 +0545

    Update newfile.c

--- src/commands/newfile.c
+++ src/commands/newfile.c
@@ -7,12 +7,6 @@ void create_file(char *filename) {
     FILE *fp = fopen(filename, "wx");
     
     if (fp == NULL) {
-        // If the error is strictly because the file already exists, do nothing silently
-        if (errno == EEXIST) {
-            return; 
-        }
-        
-        // Print any other error (e.g., "Permission denied", "No such file or directory")
         perror("open");
         return;
     }
