26eef937e1ddfc53e160626c303157acde2de8d5
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 22:05:06 +0545

    Update guide.c

--- src/utils/guide.c
+++ src/utils/guide.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include "guide.h"
 
-void display_guide(){
-	printf("\033[32m==================== Shell Guide ====================\033[0m\n");
+void display_guide() {
+    printf("Shell Guide\n\n");
 
-	printf("\n");
+    printf("Shell CLI Commands:\n");
+    printf("  shell -g, --guide     Show this guide\n");
+    printf("  shell -v, --version   Show version information\n");
+    printf("  shell -i, --info      Show information about shell\n\n");
 
-	printf("Shell CLI Commands:\n");
-	printf("  shell -i, --info      Show information of rumi\n");
-	printf("  shell -v, --version   Show version information\n");
-	printf("  shell -g, --guide     Show this guide\n");
-
-	printf("\n");
-
-	printf("Shell Commands:\n");
-	printf("  clean              - Clear the terminal screen\n");
-	printf("  esc                - Exit the shell\n");
-	printf("  list               - List files and directories in the current directory\n");
-	printf("  newfile <filename> - Create an empty file with the given name\n");
-	printf("  delfile <filename> - Delete the specified file\n");
-	printf("  newdir <dirname>   - Create a new directory with the given name\n");
-	printf("  deldir <dirname>   - Delete directory with the given name\n");
-	printf("  print <text>       - Print the specified text to the terminal\n");
-	printf("  read <filename>    - Display the contents of the specified file\n");
-	printf("  goto <pathname>    - Change the current directory to the specified path\n");
+    printf("Shell Commands:\n");
+    printf("  list               - List files and directories in the current directory\n");
+    printf("  goto <pathname>    - Change the current directory\n");
+    printf("  read <filename>    - Display the contents of a file\n");
+    printf("  print <text>       - Print text to the terminal\n");
+    printf("  newfile <filename> - Create an empty file\n");
+    printf("  delfile <filename> - Delete a file\n");
+    printf("  newdir <dirname>   - Create a new directory\n");
+    printf("  deldir <dirname>   - Delete a directory\n");
+    printf("  clean              - Clear the terminal screen\n");
+    printf("  esc                - Exit the shell\n");
 }
