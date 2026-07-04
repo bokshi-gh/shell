6b54d8a24a8851d5a40fdfffb21c71a631a583b1
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 20:56:02 +0545

    Update guide.c

--- src/utils/guide.c
+++ src/utils/guide.c
@@ -2,18 +2,18 @@
 #include "guide.h"
 
 void display_guide(){
-	printf("\033[32m==================== Rumi Guide ====================\033[0m\n");
+	printf("\033[32m==================== Shell Guide ====================\033[0m\n");
 
 	printf("\n");
 
-	printf("Rumi CLI Commands:\n");
-	printf("  rumi -i, --info      Show information of rumi\n");
-	printf("  rumi -v, --version   Show version information\n");
-	printf("  rumi -g, --guide     Show this guide\n");
+	printf("Shell CLI Commands:\n");
+	printf("  shell -i, --info      Show information of rumi\n");
+	printf("  shell -v, --version   Show version information\n");
+	printf("  shell -g, --guide     Show this guide\n");
 
 	printf("\n");
 
-	printf("Rumi Shell Commands:\n");
+	printf("Shell Commands:\n");
 	printf("  clean              - Clear the terminal screen\n");
 	printf("  esc                - Exit the shell\n");
 	printf("  list               - List files and directories in the current directory\n");
@@ -24,5 +24,4 @@ void display_guide(){
 	printf("  print <text>       - Print the specified text to the terminal\n");
 	printf("  read <filename>    - Display the contents of the specified file\n");
 	printf("  goto <pathname>    - Change the current directory to the specified path\n");
-	printf("  <non_rumi_command> - Executed as a system command (e.g., ls, gcc, vim)\n");
 }
