61588bc0a638f5db1ca1917a15db81c21545d3d5
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 22:33:38 +0545

    Update newfile.c

--- src/commands/newfile.c
+++ src/commands/newfile.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <errno.h>
+
 #include "newfile.h"
 
-void create_file(char *filename){
-	FILE *fp = fopen(filename, "w");
-	fclose(fp);
+void create_file(char *filename) {
+    FILE *fp = fopen(filename, "wx");
+    
+    if (fp == NULL) {
+        // If the error is strictly because the file already exists, do nothing silently
+        if (errno == EEXIST) {
+            return; 
+        }
+        
+        // Print any other error (e.g., "Permission denied", "No such file or directory")
+        perror("open");
+        return;
+    }
+    
+    fclose(fp);
 }
