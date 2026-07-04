aaff61a90ca679521a8671ee4c1bf34fa6ff4729
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 21:18:57 +0545

    Update macros.h

--- include/macros.h
+++ include/macros.h
@@ -1,16 +1,10 @@
 #pragma once
 
 #define VERSION "v1.0.0"
-#define AUTHOR "devRajeshThapa"
+#define AUTHOR "Rajesh Thapa (bokshi)"
 #define GITHUB "https://github.com/devrajeshthapa/shell.git"
-#define DESC "A minimal shell written in C/C++"
+#define DESC "A Tiny Shell Written in C"
 #define LICENSE "GNU General Public License v3.0"
 
 #define MAX_COMMAND_LENGTH 4096
 #define CLEAR_TERMINAL() printf("\033[2J\033[H")
-
-//ANSI escape sequence
-#define GREEN "\033[0;32m"
-#define BLUE "\033[0;34m"
-#define RESET "\033[0m"
-
