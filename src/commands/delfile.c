ba0944a5aee7688b7e98ce7051a77e48a6434abe
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 22:35:00 +0545

    Update delfile.c

--- src/commands/delfile.c
+++ src/commands/delfile.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+
 #include "delfile.h"
 
 void delete_file(char *filename){
 	if(unlink(filename) == -1) perror("unlink");
 	return;
 }
-
-// TODO:
-// ==> Delete multiple file
