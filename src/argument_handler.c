e9f087103e0a6cd809fd80a22d8b9061b0221698
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 21:54:08 +0545

    Update argument_handler.c

--- src/argument_handler.c
+++ src/argument_handler.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 #include "argument_handler.h"
 #include "macros.h"
 #include "info.h"
 #include "guide.h"
 
-void handle_argument(int argc, char *argv[]){
-	if (argc != 1) {
-		if (argc == 2) {
-			if ((strcmp(argv[1], "-v") == 0) || (strcmp(argv[1], "--version") == 0)) {
-				printf("rumi %s\n", VERSION);
-				return;
-			} else if ((strcmp(argv[1], "-i") == 0) || (strcmp(argv[1], "--info") == 0)) {
-				display_info();
-			} else if ((strcmp(argv[1], "-g") == 0) || (strcmp(argv[1], "--guide") == 0)) {
-				display_guide();
-			} else {
-				printf("rumi: Invalid argument \"%s\"\n", argv[1]);
-				printf("try: rumi -g or rumi --guide\n");
-				return;
-			}
+void handle_argument(int argc, char *argv[]) {
+    if (argc == 2) {
+        if ((strcmp(argv[1], "-v") == 0) || (strcmp(argv[1], "--version") == 0)) {
+            printf("shell %s\n", VERSION);
+        } else if ((strcmp(argv[1], "-i") == 0) || (strcmp(argv[1], "--info") == 0)) {
+            display_info();
+        } else if ((strcmp(argv[1], "-g") == 0) || (strcmp(argv[1], "--guide") == 0)) {
+            display_guide();
+        } else {
+            printf("rumi: Invalid argument \"%s\"\n", argv[1]);
+            printf("Try: rumi -g or rumi --guide\n");
+        }
 
-			return;
-		}else{
-			printf("rumi: Invalid arguments\n");
-			printf("Try: rumi -g or rumi --guide\n");
-			return;
-		}
+        return;
+    }
 
-
-		return;
-	}
+    printf("shell: Invalid arguments\n");
+    printf("Try: shell -g or shell --guide\n");
 }
