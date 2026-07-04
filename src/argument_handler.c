a6dfe6017d4312f7eb1cbdc31b2a5fad398e5971
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 20:59:08 +0545

    Update argument_handler.c

--- src/argument_handler.c
+++ src/argument_handler.c
@@ -7,16 +7,16 @@
 #include "guide.h"
 
 void handle_argument(int argc, char *argv[]){
-	if(argc != 1){
-		if(argc == 2){
-			if((strcmp(argv[1], "-v") == 0) || (strcmp(argv[1], "--version") == 0)){
+	if (argc != 1) {
+		if (argc == 2) {
+			if ((strcmp(argv[1], "-v") == 0) || (strcmp(argv[1], "--version") == 0)) {
 				printf("rumi %s\n", VERSION);
 				return;
-			}else if((strcmp(argv[1], "-i") == 0) || (strcmp(argv[1], "--info") == 0)){
+			} else if ((strcmp(argv[1], "-i") == 0) || (strcmp(argv[1], "--info") == 0)) {
 				display_info();
-			}else if((strcmp(argv[1], "-g") == 0) || (strcmp(argv[1], "--guide") == 0)){
+			} else if ((strcmp(argv[1], "-g") == 0) || (strcmp(argv[1], "--guide") == 0)) {
 				display_guide();
-			}else{
+			} else {
 				printf("rumi: Invalid argument \"%s\"\n", argv[1]);
 				printf("try: rumi -g or rumi --guide\n");
 				return;
