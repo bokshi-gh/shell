4b92b3f6a1be8ec5cd00f075d6f1acf55ca582fb
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 23:49:16 +0545

    Update list.c

--- src/commands/list.c
+++ src/commands/list.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+
 #include "macros.h"
 #include "list.h"
 
-void list_directory(char *path){
-	DIR *d = opendir(char *path);
-	struct dirent* entry;
+void list_directory(char *path) {
+    DIR *d = opendir(path);
 
-	while((entry = readdir(d)) != NULL){
-		if((strcmp(entry->d_name, ".") == 0) || (strcmp(entry->d_name, "..") == 0)) continue;
-		
-		if(entry->d_type == DT_DIR) printf("%s%s/%s  ", GREEN, entry->d_name, RESET);
-		else printf("%s  ", entry->d_name);
+    if (d == NULL) {
+        perror("opendir failed");
+        return;
+    }
 
-	}
+    struct dirent *entry;
 
-	printf("\n");
-	closedir(d);
+    while ((entry = readdir(d)) != NULL) {
+        if ((strcmp(entry->d_name, ".") == 0) ||
+            (strcmp(entry->d_name, "..") == 0)) {
+            continue;
+        }
+
+        if (entry->d_type == DT_DIR) {
+            printf("%s%s/%s  ", BLUE, entry->d_name, RESET);
+        } else {
+            printf("%s  ", entry->d_name);
+        }
+    }
+
+    printf("\n");
+    closedir(d);
 }
-
-// TODO:
-// ==> Display or hide hidden file/folder specified by the flag
-// ==> Display directory contents of given path
