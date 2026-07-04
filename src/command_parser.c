1651e81357241423edeee62078f75ef1beba6ea2
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-04 21:51:00 +0545

    Update command_parser.c

--- src/command_parser.c
+++ src/command_parser.c
@@ -1,13 +1,14 @@
 #include <string.h>
 #include "command_parser.h"
 
-void parse_command(char command[], char *command_tokens[]){
+void parse_command(char command[], char *command_tokens[]) {
 	int index = 0;
 	char *token = strtok(command, " ");
 
-	while(token != NULL){
+	while (token != NULL) {
 		command_tokens[index++] = token;
 		token = strtok(NULL, " ");
 	}
+	
 	command_tokens[index] = NULL;
 }
